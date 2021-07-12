ca139c0a49712b55fc27ae631c6a4bf3a1bd22e8
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-07-12 17:14:25 -0300

    Upando

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+
+import { MainGrid } from '../src/components/MainGrid';
+import { Box } from '../src/components/Box';
+import {AlurakutMenu} from '../src/lib/alurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+
+function ProfileSideBar(props){
+  return(
+    <Box>
+    <img src={`https://github.com/${props.githubUser}.png`} style={{borderRadius: '8px'}} />
+   </Box>
+  );
+}
+
+export default function Home() {
+  const githubUser = 'GeronimoOlanda'; // criando variavel para setar o user
+  const pessoasFavoritas = ['juunegreiros', 'GeronimoOlanda', 'peas', 'rafaballerini', 'marcobrunodev', 'felipefialho'];
+
+  return (
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        {/*"grid-area: profileArea;"*/}
+        <div className="profileArea" style={{gridArea: 'profileArea'}}>
+          <ProfileSideBar githubUser={githubUser}/>
+        </div>
+
+        <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+          <Box>
+            <h1 className="title">bem vindo</h1>
+          </Box>
+        </div>
+     
+        <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Pessoas da Comunidade ({pessoasFavoritas.length})</h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual)=>{
+               
+                return(
+                  <li>
+                    <a href={`/users/${githubUser}`} key={githubUser}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{githubUser}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+    );
+}
